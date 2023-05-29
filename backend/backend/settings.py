97fba4d4139ea5af7bc072e87d7b2817c3c14997
Татьяна Степанова <stavina91@gmail.com>
2023-05-29 22:17:23 +0300

    fix settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,8 +1,7 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
@@ -118,5 +117,5 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+    'http://localhost:3000'
 ]
